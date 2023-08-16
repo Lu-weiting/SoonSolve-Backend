20f673a29284e9aa8b3a06df3e6020b1c0f67fea
CCHTony <128220228+CCHTony@users.noreply.github.com>
2023-08-16 15:31:17 +0800

    fix weiting git problem

--- api1.0/Controllers/usersController.js
+++ api1.0/Controllers/usersController.js
@@ -4,53 +4,52 @@ const auth = require('../utils/auth')
 const tool = require('../utils/tool')
 const errorMsg = require('../utils/error');
 
-// 12231565
+
 module.exports = {
-  // signUp: async(req, res) => {
-  //   const { name, email, password } = req.body;
+  signUp: async(req, res) => {
+    const { name, email, password } = req.body;
 
-  //   // 檢查必填欄位是否都有輸入
-  //   if (!name || !password || !email) {
-  //     return errorMsg.inputEmpty(res);
-  //   }
+    // 檢查必填欄位是否都有輸入
+    if (!name || !password || !email) {
+      return errorMsg.inputEmpty(res);
+    }
   
-  //   // 檢查 email 格式是否正確
-  //   if (!tool.checkEmailRegex(email)) {
-  //     return errorMsg.wrongEmail(res);
-  //   }
-  //   const response = await usersModel.signUp(name, email, password);
-  //   return res.json(response);
-  // },
-  // signIn: async(req, res) => {
+    // 檢查 email 格式是否正確
+    if (!tool.checkEmailRegex(email)) {
+      return errorMsg.wrongEmail(res);
+    }
+    const response = await usersModel.signUp(name, email, password);
+    return res.json(response);
+  },
+  signIn: async(req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
-  //     const email = req.body.email;
-  //     const password = req.body.password;
+    // 檢查必填欄位是否都有輸入
+    if (!email || !password) {
+      return errorMsg.inputEmpty(res);
+    }
 
-  //     // 檢查必填欄位是否都有輸入
-  //     if (!email || !password) {
-  //       return errorMsg.inputEmpty(res);
-  //     }
+    const user = await usersModel.signIn(email);
 
-  //     const user = await usersModel.signIn(email);
+    // 驗證密碼是否正確
+    const PASSWORD = user.password;
+    const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
+    if (PASSWORD !== hashedPassword) {
+      return errorMsg.wrongPassword(res);
+    }
 
-  //     // 驗證密碼是否正確
-  //     const PASSWORD = user.password;
-  //     const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
-  //     if (PASSWORD !== hashedPassword) {
-  //       return errorMsg.wrongPassword(res);
-  //     }
-
-  //     // 回傳登入成功的回應，包含 JWT 和使用者資訊
-  //     const response = {
-  //       'data': {
-  //         'access_token': auth.generateJWTToken(user.id),
-  //         "user": {
-  //           ...user
-  //         }
-  //       }
-  //     };
-  //     res.json(response);
-  // },
+    // 回傳登入成功的回應，包含 JWT 和使用者資訊
+    const response = {
+      'data': {
+        'access_token': auth.generateJWTToken(user.id),
+        "user": {
+          ...user
+        }
+      }
+    };
+    res.json(response);
+  },
   getRecords: async (req, res) => {
     try {
       const my_id = req.decodedToken.id;
