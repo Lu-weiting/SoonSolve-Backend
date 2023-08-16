534c0b0cbd3f5b04deb8d5bb85904ae18b09aacb
CCHTony <128220228+CCHTony@users.noreply.github.com>
2023-08-16 16:22:04 +0800

    test signup controller

--- api1.0/Routers/usersRouter.js
+++ api1.0/Routers/usersRouter.js
@@ -9,7 +9,6 @@ router.post('/signup', usersController.signUp);
 // User Sign-in endpoint
 router.post('/signin', usersController.signIn);
 
-
 router.get('/:type/task_records', auth.verifyToken, usersController.getRecords);
 router.get('/:id/profile', auth.verifyToken, usersController.getProfile);
 module.exports = router;
