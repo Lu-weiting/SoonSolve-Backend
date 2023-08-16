0910a313ab5dfce5f294dc4c5383733c89bc19d4
Ahban08 <mary870818@yahoo.com.tw>
2023-08-16 18:00:44 +0800

    update result output

--- api1.0/Models/tasksModel.js
+++ api1.0/Models/tasksModel.js
@@ -210,26 +210,26 @@ module.exports = {
       WHERE t.id = ?
       `;
 
-            const result = await connection.execute(query, [postId]);
+            const [result] = await connection.execute(query, [postId]);
             if (result.length == 0) return errorMsg.taskNotExist(res);
             const response = {
                 data: {
                     task: {
                         id: postId,
-                        title: result.title,
-                        poster_id: result.poster_id,
-                        created_at: moment.utc(taskResult.created_at).tz('Asia/Taipei').format('YYYY-MM-DD HH:mm:ss'),
-                        closed_at: moment.utc(taskResult.closed_at).tz('Asia/Taipei').format('YYYY-MM-DD HH:mm:ss'),
-                        deadline: moment.utc(taskResult.deadline).tz('Asia/Taipei').format('YYYY-MM-DD HH:mm:ss'),
-                        task_vacancy: result.task_vacancy,
-                        approved_count: result.approved_count,
-                        location: result.location,
-                        reward: result.reward,
-                        content: result.content,
-                        name: result.name,
-                        nickname: result.nickname,
-                        picture: result.picture,
-                        status: result.status,
+                        title: result[0].title,
+                        poster_id: result[0].poster_id,
+                        created_at: moment.utc(result[0].created_at).tz('Asia/Taipei').format('YYYY-MM-DD HH:mm:ss'),
+                        closed_at: moment.utc(result[0].closed_at).tz('Asia/Taipei').format('YYYY-MM-DD HH:mm:ss'),
+                        deadline: moment.utc(result[0].deadline).tz('Asia/Taipei').format('YYYY-MM-DD HH:mm:ss'),
+                        task_vacancy: result[0].task_vacancy,
+                        approved_count: result[0].approved_count,
+                        location: result[0].location,
+                        reward: result[0].reward,
+                        content: result[0].content,
+                        name: result[0].name,
+                        nickname: result[0].nickname,
+                        picture: result[0].picture,
+                        status: result[0].status,
                     }
                 }
             };
