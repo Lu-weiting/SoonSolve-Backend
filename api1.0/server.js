dff8381b8556ae189ef8c28df0b9848e8b23caf6
CCHTony <128220228+CCHTony@users.noreply.github.com>
2023-08-17 10:29:10 +0800

    fix cors problem

--- api1.0/server.js
+++ api1.0/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors'); // 引入 CORS 套件，用於處理跨來源請求
 
 const app = express();
+
+require('dotenv').config(); // 載入 .env 檔案中的環境變數
+
 app.use(cors());
 app.use('/static', express.static('/soonsolve/static'));
 app.use(express.json()); // 使用內建的 express.json 中間件，解析請求的 JSON 資料
