23476d7da51897eab7298465f6ca1ab61a0357f6
Lu-weiting <runeeld23@gmail.com>
2023-08-23 16:44:22 +0800

    modify server.js

--- api1.0/utils/messages.js
+++ api1.0/utils/messages.js
@@ -5,6 +5,8 @@ async function formatMessage(id, username, text) {
     try {
         const [selectResult] = connection.execute('SELECT * FROM users WHERE id = ?',[id]);
         console.log(id);
+        console.log(selectResult[0].picture);
+        console.log(selectResult);
         return {
             sender_id: {
                 id: id,
@@ -19,8 +21,6 @@ async function formatMessage(id, username, text) {
     } finally {
         console.log('connection release');
     }
-    
-  
 }
 
 module.exports = formatMessage;
