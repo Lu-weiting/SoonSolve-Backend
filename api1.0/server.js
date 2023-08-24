80b547c127b65f8125cbb955c8159eb6aa4374eb
Lu-weiting <runeeld23@gmail.com>
2023-08-24 17:15:20 +0800

    log

--- api1.0/server.js
+++ api1.0/server.js
@@ -68,7 +68,7 @@ app.get('/api/1.0/', (req, res) => {
 
 const server = http.createServer(app);
 console.log("call start head");
-rbq.startWorker.catch(console.error);
+rbq.startWorker();
 console.log("start fail");
 const io = require("socket.io")(server, {
     cors: {
