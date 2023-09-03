7cdf833b0061db6cbc0a6c6d44151ed7c5d29a03
Lu-weiting <runeeld23@gmail.com>
2023-09-03 14:06:37 +0800

    modify rabbitMQ

--- api1.0/utils/rbqWorker.js
+++ api1.0/utils/rbqWorker.js
@@ -6,34 +6,44 @@ dotenv.config()
 
 module.exports = {
   startRabbitMq: async () => {
-    try {
-      console.log(`${process.env.RABBITMQ_DEFAULT_USER}//${process.env.RABBITMQ_DEFAULT_PASS}`);
-      const connection = await amqp.connect(`amqp://${process.env.RABBITMQ_DEFAULT_USER}:${process.env.RABBITMQ_DEFAULT_PASS}@192.168.0.2:5672`);
-      console.log('connect sucess');
-      const channel = await connection.createChannel();
-      console.log('createChannel sucess');
+    const maxRetries = 10;
+    let retryCount = 0;
+    let connection = null;
+    while (retryCount < maxRetries) {
+      try {
+        console.log(`${process.env.RABBITMQ_DEFAULT_USER}//${process.env.RABBITMQ_DEFAULT_PASS}`);
+        const connection = await amqp.connect(`amqp://${process.env.RABBITMQ_DEFAULT_USER}:${process.env.RABBITMQ_DEFAULT_PASS}@192.168.0.2:5672`);
+        console.log('connect sucess');
+        const channel = await connection.createChannel();
+        console.log('createChannel sucess');
 
-      const queue = 'mail_queue';
-      await channel.assertQueue(queue, { durable: false });
+        const queue = 'mail_queue';
+        await channel.assertQueue(queue, { durable: false });
 
-      console.log('mail_queue is waiting');
+        console.log('mail_queue is waiting');
 
-      channel.consume(queue, async (msg) => {
-        console.log(msg.content.toString());
-        const mailOptions = JSON.parse(msg.content.toString());
-        await sendMail(mailOptions)
-          .then(() => {
-            console.log('Email sent:', mailOptions.to);
-            channel.ack(msg); // 確認訊息已處理
-          })
-          .catch((error) => {
-            console.error('Error sending email:', error);
-            channel.reject(msg, false); // 拒絕訊息並不再重新排程
-          });
-      });
-
-    } catch (error) {
-      console.error('Error connecting to RabbitMQ:', error);
+        channel.consume(queue, async (msg) => {
+          console.log(msg.content.toString());
+          const mailOptions = JSON.parse(msg.content.toString());
+          await sendMail(mailOptions)
+            .then(() => {
+              console.log('Email sent:', mailOptions.to);
+              channel.ack(msg); // 確認訊息已處理
+            })
+            .catch((error) => {
+              console.error('Error sending email:', error);
+              channel.reject(msg, false); // 拒絕訊息並不再重新排程
+            });
+        });
+        break;
+      } catch (error) {
+        retryCount++;
+        await new Promise((resolve) => setTimeout(resolve, 5000)); // 5秒后重试
+        console.error('Error connecting to RabbitMQ:', error);
+      }
+    }
+    if (!connection) {
+      console.error('Failed to connect to RabbitMQ after max retries.');
     }
   },
   sendMail: async (mailOptions) => {
