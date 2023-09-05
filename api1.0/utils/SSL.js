b1c58c8f15b4fb1ac0b316d4c046d78e69b93854
Lu-weiting <runeeld23@gmail.com>
2023-09-05 22:29:21 +0800

    update SSL

--- api1.0/utils/SSL.js
+++ api1.0/utils/SSL.js
@@ -4,6 +4,7 @@ const path =require('path');
 
 router.get('/.well-known/pki-validation/645AC20059CE0F16BEF838BF3350EEEE.txt',(req,res)=>{
     const file= path.join(__dirname,'..','static','.well-known','pki-validation','645AC20059CE0F16BEF838BF3350EEEE.txt');
+    console.log(file);
     res.sendFile(file);
 });
 
