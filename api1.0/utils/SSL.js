70078e102c2f9d255c561ddfa3c4e6f6d7ccd68f
Lu-weiting <runeeld23@gmail.com>
2023-09-05 22:25:39 +0800

    update SSL

--- api1.0/utils/SSL.js
+++ api1.0/utils/SSL.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path =require('path');
 
-router.get('../static/.well-known/pki-validation/645AC20059CE0F16BEF838BF3350EEEE.txt',(req,res)=>{
+router.get('/.well-known/pki-validation/645AC20059CE0F16BEF838BF3350EEEE.txt',(req,res)=>{
     const file= path.join(__dirname,'..','static','.well-known','pki-validation','645AC20059CE0F16BEF838BF3350EEEE.txt');
     res.sendFile(file);
 });
